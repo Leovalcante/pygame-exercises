2194d3ef2d5e54fecc2dfd45c88edd7d1e120ae8
Valerio Preti <valerio.preti@gmail.com>
2023-10-15 15:24:15 +0200

    add trasnformation

---
+++ transformations/game.py
@@ -0,0 +1,37 @@
+import sys
+
+import pygame
+
+pygame.init()
+pygame.display.set_caption("game")
+screen = pygame.display.set_mode((1000, 500), 0, 32)
+clock = pygame.time.Clock()
+
+arrow_img = pygame.image.load("arrow.png")
+arrow_spin = 0
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
+    screen.fill("white")
+    arrow_spin += 1
+    for i in range(4):
+        arrow = arrow_img.copy()
+
+        if i == 0:
+            arrow = pygame.transform.flip(arrow, True, False)
+        elif i == 1:
+            arrow = pygame.transform.scale(arrow, (80, 10))
+        elif i == 2:
+            arrow = pygame.transform.rotate(arrow, arrow_spin)
+
+        screen.blit(arrow, (50 + i * 250, 200))
+
+    pygame.display.update()
+    clock.tick(60)
